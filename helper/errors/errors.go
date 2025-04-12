31521161dfcf1c6cbdf139d5fd62e36679545aeb
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-04-12 10:26:35 +0700

    initial commit and add helper packages

---
+++ helper/errors/errors.go
@@ -0,0 +1,17 @@
+package errors
+
+import (
+	"log"
+
+	"github.com/ahmadammarm/premier-league-api/helper/response"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SendErrorResponse(context *fiber.Ctx, message string, err error) error {
+	log.Printf("Error: %s - %v", message, err)
+	context.Status(500)
+	return context.JSON(response.Response{
+		Success: false,
+		Message: message + ": " + err.Error(),
+	})
+}
---
+++ standings/standings.go
@@ -0,0 +1,13 @@
+package standings
+
+type TeamStanding struct {
+	Position       string `json:"position"`
+	Team           string `json:"team"`
+	Played         string `json:"played"`
+	Wins           string `json:"wins"`
+	Draws          string `json:"draws"`
+	Losses         string `json:"losses"`
+	GoalDifference string `json:"goal_difference"`
+	Points         string `json:"points"`
+}
+
